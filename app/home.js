c1d138c51111d2875feb3c120c477260caa75862
kazunited66 <8635@ait.nsw.edu.au>
2024-04-09 07:15:51 +1000

    home screen

--- app/home.js
+++ app/home.js
@@ -1,11 +1,76 @@
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet,TouchableOpacity, FlatList } from "react-native"
 import { Stack } from "expo-router"
-
+import React from 'react'
+import{AntDesign} from '@expo/vector-icons'
+import colors from './Color'
+import tempData from './tempData'
+import TodoList from '../components/TodoList'
+import { StatusBar } from 'expo-status-bar'
 export default function Home( props ) {
     return (
-        <View>
-            <Stack.Screen options={{ headerShown: true }}/>
-            <Text>Home</Text>
+        <View style={styles.container}>
+        <View style={{flexDirection: "row"}}>
+           <View style={styles.divider} />
+           <Text style={styles.title}>
+              Todo<Text style={{ fontWeight: "300", color: colors.blue }}>Lists</Text>
+              
+          </Text>
+          <View style={styles.divider} />
         </View>
+
+        <View style={{marginVertical: 48}}>
+          <TouchableOpacity style={styles.addList}>
+            <AntDesign name="plus" size={16} color={colors.blue} />
+          </TouchableOpacity>
+
+          <Text style={styles.add}>Add List</Text>
+
+        </View>
+        <View style={{ height: 275, paddingLeft: 32}}>
+          <FlatList 
+          data={tempData} 
+          keyExtractor={item => item.name} 
+          horizontal={true} 
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => <TodoList list={item} />}
+          />
+        </View>
+      </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    divider: {
+      backgroundColor: colors.lightBlue,
+      height: 1,
+      flex: 1,
+      alignSelf: "center"
+    },
+    title: {
+      fontSize: 38,
+      
+      paddingHorizontal: 64
+    },
+    addList: {
+      borderWidth: 2,
+      borderColor: colors.lightBlue,
+      borderRadius: 4,
+      padding:16,
+      alignItems:"center",
+      justifyContent:"center"
+  
+    },
+    add: {
+      color: colors.blue,
+      fontWeight: "600",
+      fontSice: 14,
+      marginTop: 8
+  
+    }
+  });
